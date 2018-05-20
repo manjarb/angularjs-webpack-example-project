8e57bc9e4a71e108d8e98d46ecc3e19229e5468f
Varis <manjarb4@gmail.com>
2018-05-21 07:03:05 +0800

    p

--- src/modules/packages/components/packages-search-box/packages-search-box.component.js
+++ src/modules/packages/components/packages-search-box/packages-search-box.component.js
@@ -12,18 +12,20 @@ export const PackagesSearchBoxComponent = {
         '$scope',
         class PackagesSearchBoxController {
             constructor(ChartService, $scope) {
-                this.$scope = $scope;
+                this.scope = $scope;
                 this.$ctrl = $scope.$ctrl;
                 this.chartService = ChartService;
-                console.log(ChartService, 'ChartService');
-                console.log(this.chartService, 'this.chartService');
-                this.$scope.renderPieChart = this.renderPieChart.bind(this);
+                this.scope.isChartTrigger = false;
+                this.scope.renderPieChart = this.renderPieChart.bind(this);
             }
             $onInit() {
                 /// console.log(this.$ctrl, '$ctrl');
                 // console.log(this.$ctrl['pagesData'], 'pagesData');
             }
             renderPieChart(pack) {
+                this.scope.package.packageName = pack.packageName;
+                this.scope.isChartTrigger = true;
+
                 const ctx = document.getElementById('package-pie-chart').getContext('2d');
                 const packagesListName = [
                     `${this.$ctrl.pagesData.length} Pages`,
@@ -38,7 +40,7 @@ export const PackagesSearchBoxComponent = {
                     "#c45850"
                 ];
                 let myChart = new Chart(ctx, this.chartService.returnChartObject(packagesListName, packagesListSize, 'pie', bgColor));
-
+                this.scope.$apply();
             }
     }]
 };
